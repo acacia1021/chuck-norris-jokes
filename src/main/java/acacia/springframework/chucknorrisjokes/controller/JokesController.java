8df345911347b166a759cfb823300990546260c4
acacia1021 <acacia1021@gmail.com>
2022-12-14 12:17:30 -0500

    Fixed Controller - mapping & correct method

--- src/main/java/acacia/springframework/chucknorrisjokes/controller/JokesController.java
+++ src/main/java/acacia/springframework/chucknorrisjokes/controller/JokesController.java
@@ -1,6 +1,9 @@
 package acacia.springframework.chucknorrisjokes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import acacia.springframework.chucknorrisjokes.model.Joke;
 import acacia.springframework.chucknorrisjokes.service.JokeService;
@@ -11,13 +14,15 @@ public class JokesController extends Joke{
 
 private final JokeService jokeService;
 
+@Autowired //If you only have 1 contructor, you can keave it out
 public JokesController(JokeService jokeService){
     this.jokeService = jokeService;
 }
 
-
-public String returnJoke(){
-    return jokeService.getJoke();
+@RequestMapping({"/",""})
+public String returnJoke(Model model){
+    model.addAttribute("joke", jokeService.getJoke());
+    return "index";
 }
     
 }
